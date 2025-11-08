a748647d618f1ee40e876a9d5ad867d23a5f24f0
Nikhil Kalkhuria <151564508+nanu003@users.noreply.github.com>
2025-11-08 20:43:54 +0530

    Update StartGame.jsx

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,4 +1,4 @@
-import Dice from '../assets/dice.png';
+
 import styled from 'styled-components'
 import {Button } from '../styled/button'
 
@@ -23,7 +23,7 @@ function StartGame({toggle}) {
   return (
     <Container>
       <div>
-        <img src={Dice} alt='Dice' />
+        <img src="/dice.png" alt='Dice' />
       </div>
       <div className='content'>
         <h1>DICE GAME</h1>
